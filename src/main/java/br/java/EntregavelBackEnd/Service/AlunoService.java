47a7d20bab4db36ec21c62619a79a9cffdb9f250
951549510@adm.unifor.br <guilhermesaraivax88@gmail.com>
2022-04-20 17:05:23 -0300

    update hierarquial strutured

--- src/main/java/br/java/EntregavelBackEnd/Service/AlunoService.java
+++ src/main/java/br/java/EntregavelBackEnd/Service/AlunoService.java
@@ -4,7 +4,7 @@ import br.java.EntregavelBackEnd.Model.Aluno;
 import br.java.EntregavelBackEnd.Model.DTO.AlunoDTO;
 import br.java.EntregavelBackEnd.Repositories.AlunoRepository;
 import br.java.EntregavelBackEnd.Service.Exception.ObjectNotFoundException;
-import br.java.EntregavelBackEnd.Service.Impl.RepositoryImpl;
+import br.java.EntregavelBackEnd.Service.Impl.AlunoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,45 +12,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class AlunoService implements RepositoryImpl {
+public interface AlunoService{
 
-    @Autowired
-    private AlunoRepository aluno_repository;
+    List<AlunoDTO> getAllStudents();
+    AlunoDTO findStudent(int id);
+    void removeStudent(int id);
+    AlunoDTO createStudent(Aluno aluno);
+    AlunoDTO updateExistStudent(int id, Aluno aluno);
 
-    @Override
-    public List<AlunoDTO> getAllStudents() {
-        return AlunoDTO.generateListBaseModelAluno(this.aluno_repository.findAll());
-    }
-
-    @Override
-    public AlunoDTO findStudent(int id) {
-        Optional<Aluno> studentId = this.aluno_repository.findById(id);
-        if(studentId.isPresent()){
-            AlunoDTO studentDTO = new AlunoDTO(studentId.get());
-            return studentDTO;
-        }
-        throw new ObjectNotFoundException("objeto nao localizado = " + Aluno.class + " para o ID + " + id);
-    }
-
-    @Override
-    public void removeStudent(int id) {
-        this.aluno_repository.deleteById(id);
-    }
-
-    @Override
-    public AlunoDTO createStudent(Aluno aluno) {
-        Aluno new_student = this.aluno_repository.save(aluno);
-        return new AlunoDTO(new_student);
-    }
-
-    @Override
-    public AlunoDTO updateExistStudent(int id, Aluno aluno) {
-        Optional<Aluno> udt_student = this.aluno_repository.findById(id);
-        if(udt_student.isPresent()){
-            udt_student.get().setCursoMatriculado(aluno.getCursoMatriculado());
-            Aluno commit_student = this.aluno_repository.save(udt_student.get());
-            return new AlunoDTO(commit_student);
-        }
-        throw new ObjectNotFoundException("Objeto nao localizado = " + Aluno.class + " para o ID: " + id);
-    }
 }
